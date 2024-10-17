9b870044238f5beebc70cba8acc15e87fd0efb02
DMackRus <el16dmcr@leeds.ac.uk>
2024-10-17 15:35:33 +0100

    ade a quick test mjx script, still needs more stuff, but it can at least check we can put data on the GPU

--- test_jax.py
+++ test_jax.py
@@ -1,14 +1,14 @@
-# import jax
-# import jax.numpy as jnp
-# print(jax.devices())
-# x_gpu = jax.device_put(jnp.array([1.0, 2.0, 3.0]), device=jax.devices()[0])
-# # y_gpu = x_gpu * 2
-# # print(y_gpu)
-
 import jax
 import jax.numpy as jnp
+print(jax.devices())
+x_gpu = jax.device_put(jnp.array([1.0, 2.0, 3.0]), device=jax.devices()[0])
+y_gpu = x_gpu * 2
+print(y_gpu)
 
-x = jnp.ones((1000, 1000))
-x_gpu = jax.device_put(x, device=jax.devices()[0])
+# import jax
+# import jax.numpy as jnp
 
-print(x_gpu.device_buffer.device())  # This should print "gpu:0" if the GPU is used
+# x = jnp.ones((1000, 1000))
+# x_gpu = jax.device_put(x, device=jax.devices()[0])
+
+# print(x_gpu.device_buffer.device())  # This should print "gpu:0" if the GPU is used
